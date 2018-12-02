9dc0565d327a48bf2c82aa331c7eb28f72e3e656
Student <example@example.com>
2018-12-01 18:00:26 -0800

    write spec5 to check words with special characters

--- lib/Anagrams_Antigrams.rb
+++ lib/Anagrams_Antigrams.rb
@@ -7,9 +7,9 @@ class Anagram
     @word2 = second_word
   end
 
-  def anagram()
+  def anagram_antigram()
     if((is_word?(@word)) && (is_word?(@word2)))
-      if (@word.downcase.split("").sort.join == @word2.downcase.split("").sort.join)
+      if(is_anagram?(@word, @word2))
         return "These words are Anagrams"
       elsif (is_antigram?(@word, @word2))
         return "These words are Antigrams!"
@@ -19,29 +19,21 @@ class Anagram
     end
   end
 
+  def is_anagram?(first_word, second_word)
+    if (first_word.downcase.split("").sort.join == second_word.downcase.split("").sort.join)
+      return true
+    end
+  end
+
   def is_word?(word)
     if(word =~ /[aeiou]/)
       return true
-    else
-      return false
     end
   end
 
   def is_antigram?(first_word, second_word)
     if first_word.split("") != second_word.split("")
       return true
-    else
-      return false
     end
   end
-
-
-
-
-
-
-
-
-
-
 end
--- spec/Anagrams_Antigrams_Spec.rb
+++ spec/Anagrams_Antigrams_Spec.rb
@@ -7,21 +7,27 @@ describe("Anagram") do
 
   it("checks if two words are anagrams") do
     test_anagram = Anagram.new("weather", "wreathe")
-    expect(test_anagram.anagram()).to(eq("These words are Anagrams"))
+    expect(test_anagram.anagram_antigram()).to(eq("These words are Anagrams"))
   end
 
   it("returns two words as anagrams reguardless of case") do
     test_anagram = Anagram.new("WEAther", "wreATHE")
-    expect(test_anagram.anagram()).to(eq("These words are Anagrams"))
+    expect(test_anagram.anagram_antigram()).to(eq("These words are Anagrams"))
   end
 
   it("checks to see the anagrams are actual words") do
     test_anagram = Anagram.new("weather", "wthr")
-    expect(test_anagram.anagram()).to(eq("You need to input actual words!"))
+    expect(test_anagram.anagram_antigram()).to(eq("You need to input actual words!"))
   end
 
   it("check to see if words are antigrams") do
     test_anagram = Anagram.new("WEAther", "breaThe")
-    expect(test_anagram.anagram()).to(eq("These words are Antigrams!"))
+    expect(test_anagram.anagram_antigram()).to(eq("These words are Antigrams!"))
   end
+
+  it("deletes special character in inputs to check if words are antigrams or anagrams") do
+    test_anagram = Anagram.new("weather!", "wreathe")
+    expect(test_anagram.anagram_antigram()).to(eq("These words are Anagrams"))
+  end
+
 end
